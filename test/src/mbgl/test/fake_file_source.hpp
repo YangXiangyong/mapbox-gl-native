8de3b8d0ec025c3aacc3f70dd6b6b81cf823b952
Huyen Chau Nguyen <hello@chau-nguyen.de>
2018-10-26 21:16:35 +0200

    Revert "remove redundant stub code"
    This reverts commit 47c25a18cf2142aaf34607358ebd836ab2b533c5.

--- test/src/mbgl/test/fake_file_source.hpp
+++ test/src/mbgl/test/fake_file_source.hpp
@@ -59,7 +59,47 @@ public:
     std::list<FakeFileRequest*> requests;
 };
 
-class FakeOnlineFileSource : public FakeFileSource, public OnlineFileSource {
+class FakeOnlineFileSource : public OnlineFileSource {
+public:
+    class FakeFileRequest : public AsyncRequest {
+    public:
+        Resource resource;
+        Callback callback;
+
+        std::list<FakeFileRequest*>& list;
+        std::list<FakeFileRequest*>::iterator link;
+
+        FakeFileRequest(Resource resource_, Callback callback_, std::list<FakeFileRequest*>& list_)
+            : resource(std::move(resource_)),
+              callback(std::move(callback_)),
+              list(list_),
+              link((list.push_back(this), std::prev(list.end()))) {
+        }
+
+        ~FakeFileRequest() override {
+            list.erase(link);
+        }
+    };
+
+    std::unique_ptr<AsyncRequest> request(const Resource& resource, Callback callback) override {
+        return std::make_unique<FakeFileRequest>(resource, callback, requests);
+    }
+
+    bool respond(Resource::Kind kind, const Response& response) {
+        auto it = std::find_if(requests.begin(), requests.end(), [&] (FakeFileRequest* fakeRequest) {
+            return fakeRequest->resource.kind == kind;
+        });
+
+        if (it != requests.end()) {
+            // Copy the callback, in case calling it deallocates the AsyncRequest.
+            Callback callback_ = (*it)->callback;
+            callback_(response);
+        }
+
+        return it != requests.end();
+    }
+
+    std::list<FakeFileRequest*> requests;
 };
 
 } // namespace mbgl
